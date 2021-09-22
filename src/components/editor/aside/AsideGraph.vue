a0f1c85c0a10f800dc55f29ac4202a0d4f3bc8c5
Novout <novout@hotmail.com>
2021-09-22 01:57:56 -0300

    style(graph): lines

--- src/components/editor/aside/AsideGraph.vue
+++ src/components/editor/aside/AsideGraph.vue
@@ -1,3 +1,40 @@
 <template>
-  <p></p>
+  <div class="flex flex-col mt-5 w-full p-1 wb-text">
+    <div v-for="page in store.state.project.pages" :key="page.id">
+      <div v-for="entity in page.page" :key="entity.id">
+        <div
+          v-if="entity.type === 'heading-one'"
+          class="border-l border-black dark:border-gray-500 ml-1"
+        >
+          <p class="text-tiny ml-2 font-bold truncate cursor-pointer py-2">
+            {{ entity.raw }}
+          </p>
+        </div>
+        <div
+          v-if="entity.type === 'heading-two'"
+          class="flex items-end border-l border-black dark:border-gray-500 ml-1"
+        >
+          <div
+            class="border-b mb-2 w-6 h-1 border-black dark:border-gray-500"
+          ></div>
+          <p class="text-sm ml-2 truncate cursor-pointer">{{ entity.raw }}</p>
+        </div>
+        <div
+          v-if="entity.type === 'heading-three'"
+          class="flex items-end border-l border-black dark:border-gray-500 ml-1"
+        >
+          <div
+            class="border-b mb-2 w-10 h-1 border-black dark:border-gray-500"
+          ></div>
+          <p class="text-xs ml-2 truncate cursor-pointer">{{ entity.raw }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+  import { useStore } from 'vuex'
+
+  const store = useStore()
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,22 @@ module.exports = {
     fontFamily: {
      'raleway': ['Raleway', 'ui-sans-serif', 'system-ui'],
     },
+    fontSize: {
+      '3xs': '.25rem',
+      '2xs': '.50rem',
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'tiny': '.875rem',
+       'base': '1rem',
+       'lg': '1.125rem',
+       'xl': '1.25rem',
+       '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+       '5xl': '3rem',
+       '6xl': '4rem',
+      '7xl': '5rem',
+     },
     extend: {
       height: theme => ({
         "editor": "75vh",
