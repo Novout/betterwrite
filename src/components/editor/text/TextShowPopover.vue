a6904c19e167f3cc64c73bb9273f973c7a181ab4
Novout <novout@hotmail.com>
2021-09-21 23:20:28 -0300

    style(editor): hover popover aux

--- src/components/editor/text/TextShowPopover.vue
+++ src/components/editor/text/TextShowPopover.vue
@@ -10,7 +10,7 @@
       dark:text-gray-500
     "
   >
-    <HeroIcon class="-4 w-4">
+    <HeroIcon class="h-4 w-4 hover:text-gray-400 dark:hover:text-gray-900">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         class="h-4 w-4"
@@ -22,7 +22,10 @@
         />
       </svg>
     </HeroIcon>
-    <HeroIcon class="h-4 w-4" @click="onUpEntity">
+    <HeroIcon
+      class="h-4 w-4 hover:text-gray-400 dark:hover:text-gray-900"
+      @click="onUpEntity"
+    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         class="h-4 w-4"
@@ -36,7 +39,10 @@
         />
       </svg>
     </HeroIcon>
-    <HeroIcon class="h-4 w-4" @click="onDownEntity">
+    <HeroIcon
+      class="h-4 w-4 hover:text-gray-400 dark:hover:text-gray-900"
+      @click="onDownEntity"
+    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         class="h-4 w-4"
@@ -50,19 +56,20 @@
         />
       </svg>
     </HeroIcon>
-    <HeroIcon lass="h-4 w-4" @click="onDeleteEntity">
+    <HeroIcon
+      class="h-4 w-4 hover:text-gray-400 dark:hover:text-gray-900"
+      @click="onDeleteEntity"
+    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         class="h-4 w-4"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
+        viewBox="0 0 20 20"
+        fill="currentColor"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
+          fill-rule="evenodd"
+          d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+          clip-rule="evenodd"
         />
       </svg>
     </HeroIcon>
