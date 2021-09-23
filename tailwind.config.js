60d401ce551db60b041096ab2cdd3e728d63e033
Novout <novout@hotmail.com>
2021-09-23 02:22:53 -0300

    chore(tailwind): minify bundle with specific colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
     fontFamily: {
      'raleway': ['Raleway', 'ui-sans-serif', 'system-ui'],
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.coolGray,
     },
     fontSize: {
       '3xs': '.35rem',
