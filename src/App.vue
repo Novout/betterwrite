bd36b8d3d50fdf1c5e5f9d2cc8929e71fefbc673
Novout <novout@hotmail.com>
2021-09-22 22:30:47 -0300

    chore: append app

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script setup lang="ts">
+  import { useI18n } from 'vue-i18n'
+  import { useStart } from '@/use/start'
+
+  const { t } = useI18n()
+
+  useStart().init()
+</script>
+
+<style>
+  #app {
+    min-height: 100vh;
+    width: 100%;
+  }
+</style>
