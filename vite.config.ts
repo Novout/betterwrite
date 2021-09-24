bc4459018f81671a3d69c42fa0c692d63c3d015b
Novout <novout@hotmail.com>
2021-09-23 21:21:35 -0300

    chore(deps): remove vite-eslint

--- vite.config.ts
+++ vite.config.ts
@@ -4,22 +4,20 @@ import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import { HeadlessUiResolver } from "unplugin-vue-components/resolvers";
 import vueI18n from "@intlify/vite-plugin-vue-i18n";
-import eslintPlugin from "vite-plugin-eslint";
 
 export default defineConfig({
   base: '/better-write/',
 
   plugins: [
     vue(),
-    Components({
-      dts: true,
-      resolvers: [HeadlessUiResolver()],
-    }),
     vueI18n({
       include: path.resolve(__dirname, "./src/lang/**"),
       runtimeOnly: false
     }),
-    eslintPlugin(),
+    Components({
+      dts: true,
+      resolvers: [HeadlessUiResolver()],
+    })
   ],
 
   resolve: {
