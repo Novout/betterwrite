553a6d3b7b1c9eea5ea594cbb6448d692a8bf17e
Novout <novout@hotmail.com>
2021-09-24 21:01:18 -0300

    chore: typos

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,10 @@
 </template>
 
 <script setup lang="ts">
-  import { onMounted } from 'vue'
   import { useI18n } from 'vue-i18n'
   import { useStart } from '@/use/start'
-  import { useIndexedDB } from '@/use/storage/indexed'
 
   const { t } = useI18n()
 
   useStart().init()
-
-  onMounted(() => {})
 </script>
