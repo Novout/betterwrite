d36fdb11107c509e9958c782357091a2f716113f
Novout <novout@hotmail.com>
2021-09-27 00:56:03 -0300

    feat: error page

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,5 +1,9 @@
 <template>
-  <p>:(</p>
+  <main class="wb-base flex items-center justify-center flex-col md:flex-row">
+    <router-link to="/" class="text-3xl dark:text-gray-900 text-black"
+      >404 :(</router-link
+    >
+  </main>
 </template>
 
 <script lang="ts" setup></script>
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Editor from '@/pages/Editor.vue'
+import ErrorPage from '@/pages/404.vue'
 
-const routes = [{ path: '/', component: Editor }]
-
+const routes = [
+  { path: '/', component: Editor },
+  { path: '/:pathMatch(.*)*', component: ErrorPage },
+]
 export default createRouter({
   history: createWebHistory('/better-write/'),
   routes,
