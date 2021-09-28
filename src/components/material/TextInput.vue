11161f2e07a4b2fdc61d0b7314cc599337109d47
Novout <novout@hotmail.com>
2021-09-28 14:38:36 -0300

    feat(material): input component

---
+++ src/components/material/TextInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <input ref="inp" v-model="cmp" :class="css" />
+</template>
+
+<script setup lang="ts">
+  import { ref, computed } from 'vue'
+
+  const props = defineProps({
+    modelValue: {
+      required: true,
+      type: String,
+    },
+    css: {
+      required: false,
+      type: String,
+    },
+  })
+
+  const emit = defineEmits(['update:modelValue'])
+  const inp = ref<HTMLElement | null>(null as any)
+  const cmp = computed({
+    get() {
+      return props.modelValue
+    },
+    set(val: any) {
+      emit('update:modelValue', val)
+    },
+  })
+</script>
