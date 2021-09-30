1bd5f1c844ec66cc8cf252423091cda055861d32
Novout <novout@hotmail.com>
2021-09-29 23:11:12 -0300

    fix(select): remove svg

--- src/components/material/TextSelect.vue
+++ src/components/material/TextSelect.vue
@@ -23,30 +23,6 @@
         "
       >
         <span class="block truncate">{{ cmp }}</span>
-        <span
-          class="
-            absolute
-            inset-y-0
-            right-0
-            flex
-            items-center
-            pr-2
-            pointer-events-none
-          "
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-              clip-rule="evenodd"
-            />
-          </svg>
-        </span>
       </ListboxButton>
 
       <transition
