dddafec911b7b8872bdbd11bd35fbfd5cd9fe532
Novout <novout@hotmail.com>
2021-10-01 18:20:51 -0300

    refactor(editor): remove options in popover

--- src/components/editor/entity/EditorEntityShowPopover.vue
+++ src/components/editor/entity/EditorEntityShowPopover.vue
@@ -17,21 +17,6 @@
   >
     <HeroIcon
       class="h-4 w-4 hover:text-gray-400 dark:hover:text-gray-900"
-      @click="onOptionsEntity"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-4 w-4"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path
-          d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z"
-        />
-      </svg>
-    </HeroIcon>
-    <HeroIcon
-      class="h-4 w-4 hover:text-gray-400 dark:hover:text-gray-900"
       @click="onUpEntity"
     >
       <svg
