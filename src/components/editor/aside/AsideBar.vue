7c63145cfa91b707b94d18f1ecf7fb9b577add1d
Novout <novout@hotmail.com>
2021-10-02 23:32:58 -0300

    feat: logo in aside bar

--- src/components/editor/aside/AsideBar.vue
+++ src/components/editor/aside/AsideBar.vue
@@ -1,4 +1,5 @@
 <template>
+  <AsideBarLogo />
   <AsideBarConfiguration />
   <AsideBarProject />
 </template>
---
+++ src/components/editor/aside/AsideBarLogo.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex justify-around sm:justify-between items-center w-full p-2">
+    <img class="object-contain" width="60" src="/logo.svg" />
+    <p class="wb-text font-bold text-3xl sm:text-xl">Better Write</p>
+  </div>
+</template>
