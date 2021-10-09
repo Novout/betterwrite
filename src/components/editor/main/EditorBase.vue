180e2cddc6805cf67606f4a15202f4c422e5cdd7
Novout <novout@hotmail.com>
2021-10-08 22:51:23 -0300

    fix(editor): break overflow scrollbar

--- src/components/editor/main/EditorBase.vue
+++ src/components/editor/main/EditorBase.vue
@@ -1,49 +1,48 @@
 <template>
-  <div class="h-screen flex items-center" @click.prevent="onClickInEditor">
-    <div
-      ref="main"
-      class="w-full h-screen bg-gray-100 dark:bg-gray-700"
-      :class="[
-        store.state.editor.configuration.draggable ? 'fixed' : 'inline-block',
-      ]"
-      :style="style"
-      style="
-        box-shadow: rgba(50, 50, 105, 0.15) 0px 2px 5px 0px,
-          rgba(0, 0, 0, 0.05) 0px 1px 1px 0px;
+  <div
+    ref="main"
+    class="w-full overflow-hidden h-screen bg-gray-100 dark:bg-gray-700"
+    :class="[
+      store.state.editor.configuration.draggable ? 'fixed' : 'inline-block',
+    ]"
+    :style="style"
+    style="
+      box-shadow: rgba(50, 50, 105, 0.15) 0px 2px 5px 0px,
+        rgba(0, 0, 0, 0.05) 0px 1px 1px 0px;
+    "
+    @click.prevent="onClickInEditor"
+  >
+    <EditorBaseHeader
+      v-if="store.state.project.name !== useEnv().projectEmpty()"
+    />
+    <EditorBaseBlocked
+      v-if="store.state.project.name === useEnv().projectEmpty()"
+    />
+    <section
+      id="edit"
+      class="
+        flex flex-col
+        w-full
+        overflow-y-auto
+        max-h-screen
+        px-14
+        bg-gray-100
+        dark:bg-gray-700
       "
     >
-      <EditorBaseHeader
+      <EditorEntityShow
+        v-for="(entity, index) in store.state.context.entity"
+        :id="`entity-${String(index)}`"
+        :key="index"
+        :entity="entity"
+      />
+      <EditorEntityInput
         v-if="store.state.project.name !== useEnv().projectEmpty()"
+        v-model="entry"
+        @enter="enterListener"
+        @reset="resetListener"
       />
-      <EditorBaseBlocked
-        v-if="store.state.project.name === useEnv().projectEmpty()"
-      />
-      <section
-        id="edit"
-        class="
-          flex flex-col
-          w-full
-          overflow-y-auto
-          max-h-screen
-          px-14
-          bg-gray-100
-          dark:bg-gray-700
-        "
-      >
-        <EditorEntityShow
-          v-for="(entity, index) in store.state.context.entity"
-          :id="`entity-${String(index)}`"
-          :key="index"
-          :entity="entity"
-        />
-        <EditorEntityInput
-          v-if="store.state.project.name !== useEnv().projectEmpty()"
-          v-model="entry"
-          @enter="enterListener"
-          @reset="resetListener"
-        />
-      </section>
-    </div>
+    </section>
   </div>
 </template>
 
