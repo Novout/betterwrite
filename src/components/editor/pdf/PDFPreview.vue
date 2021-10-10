d6dbc9cdcd3b996ba18b911b4bcc992edca477cc
Novout <novout@hotmail.com>
2021-10-10 17:42:43 -0300

    fix(preview): icon full size

--- src/components/editor/pdf/PDFPreview.vue
+++ src/components/editor/pdf/PDFPreview.vue
@@ -1,19 +1,21 @@
 <template>
-  <HeroIcon class="wb-text" @click.prevent="onClick">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      class="h-6 w-6"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-        clip-rule="evenodd"
-      />
-    </svg>
-  </HeroIcon>
-  <div ref="preview" class="h-1/2"></div>
+  <section class="flex flex-col w-full">
+    <HeroIcon class="wb-icon w-7" @click.prevent="onClick">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+          clip-rule="evenodd"
+        />
+      </svg>
+    </HeroIcon>
+    <div ref="preview" class="h-3/4"></div>
+  </section>
 </template>
 
 <script setup lang="ts">
