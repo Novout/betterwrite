593ffdfe357a63ef6b606a9497092b45e4f82e31
Novout <novout@hotmail.com>
2021-10-13 12:17:18 -0300

    fix(editor): app init and destroy

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,17 @@
 <script setup lang="ts">
   import { onBeforeUnmount } from 'vue'
   import { useI18n } from 'vue-i18n'
-  import { useStore } from 'vuex'
   import { useStart } from '@/use/start'
   import { useDestroy } from '@/use/destroy'
 
   const { t } = useI18n()
-  const store = useStore()
 
-  useStart().init()
+  const start = useStart()
+  const destroy = useDestroy()
+
+  start.init()
 
   onBeforeUnmount(async () => {
-    await useDestroy(store).init()
+    await destroy.init()
   })
 </script>
