dad93a35a7d45a11cb435eb9fbc5b68828c4187b
Novout <novout@hotmail.com>
2021-10-13 12:48:56 -0300

    fix: inject typo

--- src/use/graph.ts
+++ src/use/graph.ts
@@ -5,11 +5,12 @@ import { useScroll } from './scroll'
 
 export const useGraph = () => {
   const store = useStore()
+  const scroll = useScroll()
 
   const load = async (go: string | symbol, page: ContextState) => {
     store.commit('context/load', page)
     await nextTick
-    useScroll().to(String(go))
+    scroll.to(String(go))
   }
 
   return { load }
