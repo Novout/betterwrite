7719f50d5573285bf71e0eed7ad408017234630d
Novout <novout@hotmail.com>
2021-10-16 19:11:18 -0300

    feat(editor): bw logo redirect to main route

--- src/components/editor/aside/AsideBarLogo.vue
+++ src/components/editor/aside/AsideBarLogo.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="flex justify-around sm:justify-between items-center w-full p-2">
-    <img class="object-contain" width="60" src="/logo.svg" />
+    <img
+      class="object-contain cursor-pointer"
+      width="60"
+      src="/logo.svg"
+      @click.stop.prevent="onClick"
+    />
     <p class="wb-text font-bold text-3xl sm:text-xl">Better Write</p>
   </div>
 </template>
+
+<script setup lang="ts">
+  import { useRouter } from 'vue-router'
+
+  const router = useRouter()
+
+  const onClick = () => {
+    router.push('/')
+  }
+</script>
