2403dfb13b75da905ae240285c4d8c612cf7b52f
Novout <novout@hotmail.com>
2021-10-19 19:02:06 -0300

    fix(editor): v-cloak in logo

--- src/components/editor/aside/AsideBarLogo.vue
+++ src/components/editor/aside/AsideBarLogo.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="flex justify-around sm:justify-between items-center w-full p-2">
+  <div
+    v-cloak
+    class="flex justify-around sm:justify-between items-center w-full p-2"
+  >
     <img
       class="object-contain cursor-pointer"
       width="60"
