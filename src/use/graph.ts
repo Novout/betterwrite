54d9b027ca17b26c0e6ef7b0cdbcc4e4775a3be7
Novout <novout@hotmail.com>
2021-10-27 12:15:49 -0300

    fix(project): normalize context items

--- src/use/graph.ts
+++ src/use/graph.ts
@@ -3,27 +3,23 @@ import { nextTick } from 'vue'
 import { useScroll } from './scroll'
 import { useContextStore } from '@/store/context'
 import useEmitter from './emitter'
+import { useProject } from './project'
 
 export const useGraph = () => {
   const CONTEXT = useContextStore()
 
   const emitter = useEmitter()
   const scroll = useScroll()
+  const project = useProject()
 
   const load = async (go: string | symbol, page: ContextState) => {
-    // close open entities contents
-    emitter.emit('entity-edit-save')
-    // force last input in emit content
-    emitter.emit('entity-input-force-enter')
-    await nextTick
-    // close all entities for not breaking same index in next page
-    emitter.emit('entity-edit-reset')
-    await nextTick
-    // load page target
-    CONTEXT.load(page)
-    await nextTick
-    // force scroll to element clicked in aside graph
-    scroll.to(String(go))
+    project.normalize().then(async () => {
+      // load page target
+      CONTEXT.load(page)
+      await nextTick
+      // force scroll to element clicked in aside graph
+      scroll.to(String(go))
+    })
   }
 
   return { load }
--- src/use/project.ts
+++ src/use/project.ts
@@ -3,24 +3,28 @@ import { useToast } from 'vue-toastification'
 import i18n from '@/lang'
 import { useProjectStore } from '@/store/project'
 import { useContextStore } from '@/store/context'
+import useEmitter from './emitter'
 
 export const useProject = () => {
   const PROJECT = useProjectStore()
   const CONTEXT = useContextStore()
 
   const toast = useToast()
+  const emitter = useEmitter()
   const { t } = i18n.global
 
-  const create = async (project: Record<string, any>) => {
-    project.type === 'blank'
-      ? PROJECT.createBlank(project)
-      : PROJECT.create(project)
+  const create = (project: Record<string, any>) => {
+    normalize().then(async () => {
+      project.type === 'blank'
+        ? PROJECT.createBlank(project)
+        : PROJECT.create(project)
 
-    await nextTick
+      await nextTick
 
-    CONTEXT.load(PROJECT.pages[0])
+      CONTEXT.load(PROJECT.pages[0])
 
-    toast.success(t('toast.project.create'))
+      toast.success(t('toast.project.create'))
+    })
   }
 
   const isBlankProject = () => {
@@ -31,5 +35,18 @@ export const useProject = () => {
     return PROJECT.type === 'creative'
   }
 
-  return { create, isBlankProject, isCreativeProject }
+  const normalize = async () => {
+    // close open entities contents
+    emitter.emit('entity-edit-save')
+    // force last input in emit content
+    emitter.emit('entity-input-force-enter')
+
+    await nextTick
+    // close all entities for not breaking same index in next page
+    emitter.emit('entity-edit-reset')
+
+    await nextTick
+  }
+
+  return { create, isBlankProject, isCreativeProject, normalize }
 }
