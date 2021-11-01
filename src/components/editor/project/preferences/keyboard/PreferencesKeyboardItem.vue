f6ad4862fc186dd1381655fd2c98015d318f12b7
Novout <novout@hotmail.com>
2021-10-31 21:58:11 -0300

    style(editor): preferences hotkeys item

--- src/components/editor/project/preferences/keyboard/PreferencesKeyboardItem.vue
+++ src/components/editor/project/preferences/keyboard/PreferencesKeyboardItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-row justify-between items-center w-full my-2">
     <h3>{{ props.title }}</h3>
-    <p class="px-2 py-1 rounded-full dark:bg-gray-800 bg-gray-400">
+    <p class="px-2 py-1 rounded-full dark:bg-gray-800 bg-gray-200">
       {{ props.shortcut }}
     </p>
   </div>
