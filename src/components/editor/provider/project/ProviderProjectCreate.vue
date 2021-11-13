90a05c60fd3bccc77cbd78f0d4364938af0b3816
Novout <novout@hotmail.com>
2021-11-12 22:29:29 -0300

    refactor(project): new with contents

--- src/components/editor/provider/project/ProviderProjectCreate.vue
+++ src/components/editor/provider/project/ProviderProjectCreate.vue
@@ -28,12 +28,28 @@
         bg-theme-background-1
         h-screen
         md:h-3/4
-        justify-evenly
+        justify-between
         flex flex-col
         overflow-y-auto
         p-5
       "
     >
+      <div class="flex w-full flex-row-reverse">
+        <HeroIcon class="wb-icon" @click="ABSOLUTE.project.new = false">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </HeroIcon>
+      </div>
       <div class="flex flex-row justify-between w-full p-2">
         <div
           v-motion
@@ -126,7 +142,7 @@
         <label class="font-bold">{{
           t('editor.aside.project.new.type')
         }}</label>
-        <div class="break-words w-96 py-1">
+        <div class="break-words w-full py-1">
           {{ typeDescription }}
         </div>
         <div
