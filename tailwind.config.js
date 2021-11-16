e9ed8880b385f03a4318092b0f973dd5c75bc3e7
Novout <novout@hotmail.com>
2021-11-16 11:38:37 -0300

    style(editor): ascend graph line

--- tailwind.config.js
+++ tailwind.config.js
@@ -198,6 +198,7 @@ module.exports = {
         "wb-landing-fourth": `url("data:image/svg+xml,%3Csvg width='44' height='12' viewBox='0 0 44 12' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M20 12v-2L0 0v10l4 2h16zm18 0l4-2V0L22 10v2h16zM20 0v8L4 0h16zm18 0L22 8V0h16z' fill='%23f9fafb' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E")`
       }),
       zIndex: theme => ({
+        "umax": "999999",
         "max": "9999",
         "preview": "200",
         "aside": "100",
