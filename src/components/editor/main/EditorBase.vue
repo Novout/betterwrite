ae6066c239532259482eee96d2a3f2f809c9c332
Novout <novout@hotmail.com>
2021-11-26 17:08:24 -0300

    chore(components): render editor

--- src/components/editor/main/EditorBase.vue
+++ src/components/editor/main/EditorBase.vue
@@ -18,74 +18,20 @@
   >
     <EditorBaseHeader />
     <EditorBaseBlocked v-if="PROJECT.name === env.projectEmpty()" />
-    <section
-      id="edit"
-      :class="[project.isBlankProject() ? 'pt-28' : '']"
-      class="
-        flex flex-col
-        w-full
-        min-h-editor
-        overflow-y-auto overflow-x-hidden
-      "
-    >
-      <EditorEntityDefaultShow
-        v-for="(entity, index) in entities"
-        :id="`entity-${String(index)}`"
-        :key="index"
-        :entity="entity"
-      />
-      <EditorEntityDefaultInput
-        v-if="PROJECT.name !== env.projectEmpty()"
-        v-model="entry"
-        @enter="enterListener"
-        @reset="resetListener"
-      />
-    </section>
+    <EditorBaseRender />
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { ref, nextTick, computed } from 'vue'
-  import { Entity } from '@/types/context'
-  import { useScroll } from '@/use/scroll'
+  import { ref } from 'vue'
   import { useEnv } from '@/use/env'
-  import { useProject } from '@/use/project'
-  import { useContextStore } from '@/store/context'
   import { useProjectStore } from '@/store/project'
   import { useEditorStore } from '@/store/editor'
-  import usePlugin from '@/use/plugin/core'
 
-  const CONTEXT = useContextStore()
   const PROJECT = useProjectStore()
   const EDITOR = useEditorStore()
 
-  const project = useProject()
-  const plugin = usePlugin()
   const env = useEnv()
 
   const main = ref<HTMLElement | null>(null)
-  const entry = ref<string>('')
-
-  const entities = computed(() => CONTEXT.entities)
-
-  const enterListener = async (content: Entity) => {
-    CONTEXT.addInPage(content)
-
-    await nextTick
-
-    useScroll().force('#edit')
-
-    resetListener()
-
-    await nextTick
-
-    plugin.emit('plugin-entity-create', {
-      data: content.raw,
-      index: CONTEXT.entities.indexOf(content),
-    })
-  }
-
-  const resetListener = () => {
-    entry.value = ''
-  }
 </script>
---
+++ src/components/editor/main/render/EditorBaseRender.vue
@@ -0,0 +1,3 @@
+<template>
+  <EditorBaseRenderDefault />
+</template>
