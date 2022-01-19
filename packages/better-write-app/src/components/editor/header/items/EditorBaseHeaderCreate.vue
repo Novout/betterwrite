06121f28bbb9f492047a9d03c8c9f8e178b03f11
Giovane Cardoso <novout@hotmail.com>
2022-01-19 18:43:52 -0300

    chore(editor): header generator to generic

--- packages/better-write-app/src/components/editor/header/items/EditorBaseHeaderCreate.vue
+++ packages/better-write-app/src/components/editor/header/items/EditorBaseHeaderCreate.vue
@@ -23,25 +23,8 @@
         :text="t('editor.bar.pdf.configuration')"
         shortcut="CTRL + G"
         @action="ABSOLUTE.pdf.configuration = true"
-      >
-        <template #icon>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            aria-hidden="true"
-            role="img"
-            width="24"
-            height="24"
-            preserveAspectRatio="xMidYMid meet"
-            viewBox="0 0 24 24"
-          >
-            <path
-              d="M22 2H6v16h16V2zm-10.5 9H9v2H7.5V7h4v4zm5 .5c0 .83-.67 1.5-1.5 1.5h-2.5V7H15c.83 0 1.5.67 1.5 1.5v3zm4-3H19v1h1.5V11H19v2h-1.5V7h3v1.5zM9 9.5h1v-1H9v1zM4 6H2v16h16v-2H4V6zm10 5.5h1v-3h-1v3z"
-              fill="currentColor"
-            ></path>
-          </svg>
-        </template>
-      </EditorHeaderItem>
+      />
+      <EditorHeaderItemDiv />
       <EditorHeaderItem
         :text="t('editor.bar.pdf.preview')"
         shortcut="CTRL + Shift + G"
