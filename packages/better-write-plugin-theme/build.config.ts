bee08af7571a9dffacc8bab130e0921d19671529
Giovane Cardoso <novout@hotmail.com>
2022-03-09 16:22:44 -0300

    chore(deps): upgrade unbuild to 0.7.0

--- packages/better-write-plugin-theme/build.config.ts
+++ packages/better-write-plugin-theme/build.config.ts
@@ -1,6 +1,9 @@
 import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
+	rollup: {
+		emitCJS: true,
+	},
 	declaration: true,
 	entries: [{ input: 'src/index.ts', outDir: 'dist', name: 'index' }],
 	externals: []
