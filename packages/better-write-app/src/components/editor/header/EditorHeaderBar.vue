fac9805f23808cedaa4f2afbfc76489ace8bb7be
Giovane Cardoso <novout@hotmail.com>
2022-03-13 16:49:36 -0300

    style(editor): header content in overflow

--- packages/better-write-app/src/components/editor/header/EditorHeaderBar.vue
+++ packages/better-write-app/src/components/editor/header/EditorHeaderBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="absolute shadow-xl rounded top-8 flex flex-col w-48 md:w-52 lg:w-72 z-50 bg-theme-editor-header-list-background"
+    class="absolute shadow-xl rounded top-8 md:top-9 lg:top-10 flex flex-col w-48 md:w-52 lg:w-72 z-50 bg-theme-editor-header-list-background"
   >
     <slot />
   </div>
