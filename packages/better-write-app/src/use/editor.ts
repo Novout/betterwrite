b5aebbf4d1eefa29e280ba83d7ca80077a645919
Giovane Cardoso <novout@hotmail.com>
2022-03-20 12:46:00 -0300

    fix(editor): event listener in save unmount

--- packages/better-write-app/src/use/editor.ts
+++ packages/better-write-app/src/use/editor.ts
@@ -4,10 +4,10 @@ import { useEntity } from '@/use/entity'
 import { useEnv } from '@/use/env'
 import { useProject } from '@/use/project'
 import { useLocalStorage } from '@/use/storage/local'
-import { useFullscreen } from '@vueuse/core'
+import { useEventListener, useFullscreen } from '@vueuse/core'
 import { useHead } from '@vueuse/head'
 import { usePlugin } from 'better-write-plugin-core'
-import { computed, onBeforeUnmount, onMounted } from 'vue'
+import { computed, onMounted } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { onBeforeRouteLeave } from 'vue-router'
 import { useListener } from './listener'
@@ -34,8 +34,8 @@ export const useEditor = () => {
       await local.onSaveProject()
     })
 
-    onBeforeUnmount(async () => {
-      await local.onSaveProject()
+    useEventListener('beforeunload', () => {
+      local.onSaveProject()
     })
 
     listener.keyboard().add()
