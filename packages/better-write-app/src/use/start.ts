94550be0446d9aaec38bd06adb442f77e0ad15d3
Giovane Cardoso <novout@hotmail.com>
2022-03-29 21:29:57 -0300

    chore: seo enhancements

--- packages/better-write-app/src/use/start.ts
+++ packages/better-write-app/src/use/start.ts
@@ -138,42 +138,42 @@ export const useStart = () => {
 
   const head = () => {
     useHead({
-      title: computed(() => t('seo.editor.title')),
+      title: computed(() => t('seo.landing.title')),
       meta: [
         {
           name: 'description',
-          content: computed(() => t('seo.editor.description')),
+          content: computed(() => t('seo.landing.description')),
         },
         ...VueI18nSEO,
         { name: 'twitter:card', content: 'summary_large_image' },
         { name: 'twitter:site', content: '@betterwriteio' },
         {
           name: 'twitter:title',
-          content: computed(() => t('seo.editor.title')),
+          content: computed(() => t('seo.landing.title')),
         },
         {
           name: 'twitter:description',
-          content: computed(() => t('seo.editor.description')),
+          content: computed(() => t('seo.landing.description')),
         },
-        { name: 'twitter:image', content: env.getProdUrl() + '/logo.png' },
+        { name: 'twitter:image', content: 'https://i.imgur.com/yvzMkqO.png' },
         {
           name: 'twitter:image:alt',
-          content: computed(() => t('seo.editor.alt')),
+          content: computed(() => t('seo.landing.alt')),
         },
         {
           property: 'og:title',
-          content: computed(() => t('seo.editor.title')),
+          content: computed(() => t('seo.landing.title')),
         },
         {
           property: 'og:description',
-          content: computed(() => t('seo.editor.description')),
+          content: computed(() => t('seo.landing.description')),
         },
         { property: 'og:url', content: env.getProdUrl() },
         { property: 'og:type', content: 'website' },
-        { property: 'og:image', content: env.getProdUrl() + '/logo.png' },
+        { property: 'og:image', content: 'https://i.imgur.com/yvzMkqO.png' },
         {
           property: 'og:image:alt',
-          content: computed(() => t('seo.editor.alt')),
+          content: computed(() => t('seo.landing.alt')),
         },
         { property: 'og:image:width', content: '500' },
         { property: 'og:image:height', content: '500' },
