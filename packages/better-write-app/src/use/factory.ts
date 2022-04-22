03839324d3302b3b8c5567b2cfc7efb4aa1d8c37
Giovane Cardoso <novout@hotmail.com>
2022-04-21 23:31:43 -0300

    fix(pdf): full drau image raw

--- packages/better-write-app/src/use/factory.ts
+++ packages/better-write-app/src/use/factory.ts
@@ -135,7 +135,7 @@ export const useFactory = () => {
                 width: 100,
                 height: 100,
               },
-              alignment: 'auto',
+              alignment: 'full',
             },
           },
         }
