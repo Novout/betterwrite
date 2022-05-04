fd6ac5b6d3d243fc605807bd5132a64cda49839c
Giovane Cardoso <novout@hotmail.com>
2022-05-04 02:28:15 -0300

    feat(entity): initial heading block item

--- packages/better-write-app/src/components/page/editor/entity/default/blocks/EditorEntityDefaultHeading.vue
+++ packages/better-write-app/src/components/page/editor/entity/default/blocks/EditorEntityDefaultHeading.vue
@@ -1,13 +1,55 @@
 <template>
   <EditorEntityDefaultContainer :entity="props.entity">
-    <EditorEntityDefaultInput :entity="props.entity" />
+    <div
+      class="flex items-center justify-center text-center w-full cursor-text"
+    >
+      <h2
+        :contenteditable="true"
+        :spellcheck="true"
+        class="font-bold"
+        :class="[
+          props.entity.type === 'heading-one'
+            ? 'text-2xl pb-10 pt-10 text-theme-editor-entity-heading-one hover:text-theme-editor-entity-heading-one-hover active:text-theme-editor-entity-heading-one-active'
+            : '',
+          props.entity.type === 'heading-two'
+            ? 'text-xl pb-3 pt-8 text-theme-editor-entity-heading-two hover:text-theme-editor-entity-heading-two-hover active:text-theme-editor-entity-heading-two-active'
+            : '',
+          props.entity.type === 'heading-three'
+            ? 'text-lg pb-2 pt-5 text-theme-editor-entity-heading-three hover:text-theme-editor-entity-heading-three-hover active:text-theme-editor-entity-heading-three-active'
+            : '',
+        ]"
+        @keypress.enter.prevent.stop="onEnter"
+      >
+        {{ props.entity.raw }}
+      </h2>
+    </div>
   </EditorEntityDefaultContainer>
 </template>
 
 <script setup lang="ts">
+  import { useContextStore } from '@/store/context'
+  import useEmitter from '@/use/emitter'
   import { Entity } from 'better-write-types'
+  import { computed, nextTick } from 'vue'
 
   const props = defineProps<{
     entity: Entity
   }>()
+
+  const CONTEXT = useContextStore()
+
+  const emitter = useEmitter()
+
+  const _index = computed(() => CONTEXT.entities.indexOf(props.entity))
+
+  const onEnter = async () => {
+    CONTEXT.newInPagePosEdit({
+      entity: props.entity,
+      type: 'paragraph',
+    })
+
+    await nextTick
+
+    emitter.emit('entity-text-focus', _index.value + 1)
+  }
 </script>
