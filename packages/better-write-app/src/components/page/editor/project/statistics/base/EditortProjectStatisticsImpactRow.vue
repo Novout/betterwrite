655bac4a7a90fa0431836185e3d4ee82f9f8dc83
Giovane Cardoso <novout@hotmail.com>
2022-05-29 21:59:29 -0300

    chore(editor): statistics impact animation

--- packages/better-write-app/src/components/page/editor/project/statistics/base/EditortProjectStatisticsImpactRow.vue
+++ packages/better-write-app/src/components/page/editor/project/statistics/base/EditortProjectStatisticsImpactRow.vue
@@ -3,6 +3,12 @@
     <h2 class="wb-text mr-2 w-52 truncate font-bold">{{ title }}</h2>
     <div class="w-full">
       <div
+        v-motion
+        :initial="{ width: `0%` }"
+        :enter="{
+          width: `${value}%`,
+          transition: { delay: 100, duration: 1200 },
+        }"
         class="h-4 bg-theme-background-opacity-1"
         :style="{ width: `${value}%` }"
       />
