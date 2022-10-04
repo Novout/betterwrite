4a5d7c3b2aea580f5653bfdab0713d81e112cf13
Novout <novout@hotmail.com>
2022-10-04 12:43:58 -0300

    chore: typos

--- packages/better-write-app/e2e/integration/landing.ts
+++
@@ -1,9 +0,0 @@
-// https://docs.cypress.io/api/introduction/api.html
-
-describe('Landing', () => {
-  it('Shows correct title text', () => {
-    cy.visit('/')
-
-    cy.contains('h1', 'Editor para escritores criativos.')
-  })
-})
--- packages/better-write-app/e2e/plugins/index.ts
+++
@@ -1,7 +0,0 @@
-import { startDevServer } from '@cypress/vite-dev-server'
-
-module.exports = (on, config) => {
-  on('dev-server:start', (options) => startDevServer({ options }))
-
-  return config
-}
