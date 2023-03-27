960c7b262eebc268ca5e31edde9851e393976ec9
Novout <novout@hotmail.com>
2023-03-27 00:14:02 -0300

    fix(editor): preferences aside bar

--- packages/better-write-app/src/components/page/editor/project/preferences/EditorProjectPreferencesContainer.vue
+++ packages/better-write-app/src/components/page/editor/project/preferences/EditorProjectPreferencesContainer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex-1 w-full wb-scroll overflow-x-scroll">
+  <div class="flex-1 w-full wb-scroll h-200 overflow-x-scroll overflow-y-auto">
     <EditorProjectPreferencesContainerProject v-if="section === 0" />
     <EditorProjectPreferencesContainerConfiguration v-if="section === 1" />
     <EditorProjectPreferencesContainerStyles v-if="section === 2" />
