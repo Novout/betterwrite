46614c9f446bacff30fd11b38fb141e992afc773
Novout <novout@hotmail.com>
2023-03-31 16:15:29 -0300

    fix(editor): comment on close strategy

--- packages/better-write-app/src/components/page/editor/entity/default/comment/EditorEntityDefaultComment.vue
+++ packages/better-write-app/src/components/page/editor/entity/default/comment/EditorEntityDefaultComment.vue
@@ -45,7 +45,7 @@
 <script setup lang="ts">
   import { ref, onMounted, nextTick, computed } from 'vue'
   import { useAbsoluteStore } from '@/store/absolute'
-  import { useDraggable } from '@vueuse/core'
+  import { tryOnUnmounted, useDraggable } from '@vueuse/core'
   import { useEditorStore } from '@/store/editor'
   import { useContextStore } from '@/store/context'
   import { useEnv } from '@/use/env'
@@ -93,9 +93,11 @@
 
   onMounted(() => {
     search.value?.focus()
+  })
 
+  tryOnUnmounted(() => {
     emitter.on('entity-external-comment-save', () => {
-      onClose()
+      
     })
   })
 
