f9ab5356ea44101d63d4762d1c11612ee15551a3
Novout <novout@hotmail.com>
2023-03-31 13:31:32 -0300

    fix(header): remove out supabase

--- packages/better-write-app/src/components/page/editor/header/items/EditorBaseHeaderLogin.vue
+++ packages/better-write-app/src/components/page/editor/header/items/EditorBaseHeaderLogin.vue
@@ -1,23 +1,23 @@
 <template>
   <div>
     <div
-      class="flex items-center px-3 font-poppins py-1 sm:py-1 bg-black-opacity border-theme-border-1 wb-text rounded-full"
+      class="flex justify-center items-center font-poppins wb-text rounded-full"
     >
       <div class="flex items-center truncate">
         <UseImage
           alt="Profile Logo"
-          class="rounded-full mr-0 sm:mr-2 w-8"
+          class="rounded-full w-8"
           :src="user?.user_metadata.avatar_url"
         >
           <template #loading>
             <Spinner
-              class="rounded-full mr-0 sm:mr-2"
+              class="rounded-full"
               :height="32"
               :width="32"
             />
           </template>
           <template #error>
-            <div class="rounded-full mr-0 sm:mr-2 w-8">
+            <div class="rounded-full w-8">
               <IconGoogle
                 v-if="user && user.app_metadata.provider === 'google'"
                 class="h-8 w-8"
@@ -38,12 +38,6 @@
             </div>
           </template>
         </UseImage>
-        <HeroIcon 
-          class="wb-icon ml-2"
-          @click.prevent.stop="supabase.out"
-        >
-          <IconClose class="w-6 h-6" />
-        </HeroIcon>
       </div>
     </div>
   </div>
@@ -51,12 +45,9 @@
 
 <script setup lang="ts">
   import { useAuthStore } from '@/store/auth'
-  import { useSupabase } from '@/use/storage/supabase'
   import { UseImage } from '@vueuse/components'
   import { computed } from 'vue'
 
   const AUTH = useAuthStore()
   const user = computed(() => AUTH.account.user)
-
-  const supabase = useSupabase()
 </script>
