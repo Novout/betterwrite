9e945b02915bd0dde5a896b1682dc6baac167f2d
Novout <novout@hotmail.com>
2023-06-10 14:41:56 -0300

    chore(editor): remove use-image component

--- packages/app/src/components/page/editor/header/items/EditorBaseHeaderUser.vue
+++ packages/app/src/components/page/editor/header/items/EditorBaseHeaderUser.vue
@@ -1,32 +1,22 @@
 <template>
   <Menu as="div" class="relative">
-    <MenuButton class="flex justify-center items-center rounded-full w-8"
-      ><UseImage class="rounded-full" :src="user?.user_metadata.avatar_url">
-        <template #loading>
-          <Spinner class="rounded-full" :height="32" :width="32" />
-        </template>
-        <template #error>
-          <div class="rounded-full wb-icon w-8 shadow-xl">
-            <IconGoogle
-              v-if="user && user.app_metadata.provider === 'google'"
-              class="h-8 w-8"
-            />
-            <IconGithub
-              v-else-if="user && user.app_metadata.provider === 'github'"
-              class="h-8 w-8"
-            />
-            <IconNotion
-              v-else-if="user && user.app_metadata.provider === 'notion'"
-              class="h-8 w-8"
-            />
-            <IconGitlab
-              v-else-if="user && user.app_metadata.provider === 'gitlab'"
-              class="h-8 w-8"
-            />
-            <IconUser v-else class="h-8 w-8" />
-          </div>
-        </template> </UseImage
-    ></MenuButton>
+    <MenuButton class="flex justify-center items-center rounded-full w-8">
+      <div class="rounded-full wb-icon w-8 shadow-xl">
+        <IconGoogle
+          v-if="user && user.app_metadata.provider === 'google'"
+          class="h-8 w-8"
+        />
+        <IconGithub
+          v-else-if="user && user.app_metadata.provider === 'github'"
+          class="h-8 w-8"
+        />
+        <IconNotion
+          v-else-if="user && user.app_metadata.provider === 'notion'"
+          class="h-8 w-8"
+        />
+        <IconUser v-else class="h-8 w-8" />
+      </div>
+    </MenuButton>
     <MenuItems
       v-motion
       :initial="{ opacity: 0, y: -10 }"
@@ -79,7 +69,6 @@
 
 <script setup lang="ts">
   import { useAuthStore } from '@/store/auth'
-  import { UseImage } from '@vueuse/components'
   import { usePlugin } from 'better-write-plugin-core'
   import { computed } from 'vue'
   import { useI18n } from 'vue-i18n'
