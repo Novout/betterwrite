3d3ef9b2a86f880e9fed6a29b757b1402a88e57c
Novout <novout@hotmail.com>
2026-06-10 19:38:08 -0300

    refactor: remove head dynamic set

--- packages/plugin-editor-window/src/seo.ts
+++ packages/plugin-editor-window/src/seo.ts
@@ -7,22 +7,11 @@ export const PluginSeoSet = (
   hooks: PluginTypes.PluginHooks,
 ) => {
   emitter.on('call-editor-created', () => {
-    // dynamic head title
     const title = computed(() => hooks.i18n.t('seo.editor.title'))
     const description = computed(() => hooks.i18n.t('seo.editor.description'))
-    const _title = computed(() =>
-      stores.PROJECT.nameRaw === hooks.env.projectEmpty() ||
-      !stores.CONTEXT.entities[0] ||
-      stores.CONTEXT.entities[0].raw === hooks.env.emptyLine() ||
-      hooks.entity.utils().isFixed(0)
-        ? title.value
-        : stores.PROJECT.nameRaw +
-          (stores.CONTEXT.entities[0]?.raw ? ' - ' : '') +
-          stores.CONTEXT.entities[0]?.raw,
-    )
 
     hooks.vueuse.head({
-      title: _title,
+      title,
       meta: [
         {
           name: `description`,
