ead66f46c23da30e9e6cb7db7cd3cd1db9f34ed0
Novout <novout@hotmail.com>
2026-06-14 18:24:39 -0300

    ci: fix stubs in runner text

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vitest/config'
 import { fileURLToPath } from 'url'
 
+const pkg = (path: string) =>
+  fileURLToPath(new URL(`./packages/${path}`, import.meta.url))
+
 export default defineConfig({
   test: {
     environment: 'happy-dom',
@@ -10,7 +13,16 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./packages/app/src', import.meta.url)),
+      '@': pkg('app/src'),
+      'better-write-types': pkg('types/src/index.ts'),
+      'better-write-languages': pkg('languages/src/index.ts'),
+      'better-write-plugin-core': pkg('plugin-core/src/index.ts'),
+      'better-write-plugin-theme': pkg('plugin-theme/src/index.ts'),
+      'better-write-client-storage': pkg('client-storage/src/index.ts'),
+      'better-write-contenteditable-ast': pkg('contenteditable-ast/src/index.ts'),
+      'better-write-extension': pkg('extension/src/index.ts'),
+      'better-write-image-converter': pkg('image-converter/src/index.ts'),
+      'better-write-plugin-importer': pkg('plugin-importer/src/index.ts'),
     },
   },
 })
